e36afb875ab208779855ad01f9ea5c0a01f30a7c
Andrew Wihardja <andrewadi96@gmail.com>
2023-12-02 01:31:01 -0800

    Added React state, to toggle between different modes at the click of a menu button

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main.css";
 import heart from "./heart_empty-removebg-preview.png";
 
 function Main() {
+  const [mode, setMode] = useState("mode1");
+
+  // Toggle between different modes at the click of a button
+  const handleMenuButtonClick = (selectedMode) => {
+    setMode(selectedMode);
+  };
+
   return (
     <div className="main-app">
       <div className="menu">
-        <button>Button1</button>
-        <button>Button2</button>
-        <button>Button3</button>
-        <button>Button4</button>
+        <button
+          className={mode === "mode1" ? "active" : ""}
+          onClick={() => handleMenuButtonClick("mode1")}
+        >
+          Button1
+        </button>
+        <button
+          className={mode === "mode2" ? "active" : ""}
+          onClick={() => handleMenuButtonClick("mode2")}
+        >
+          Button2
+        </button>
+        <button
+          className={mode === "mode3" ? "active" : ""}
+          onClick={() => handleMenuButtonClick("mode3")}
+        >
+          Button3
+        </button>
+        <button
+          className={mode === "mode4" ? "active" : ""}
+          onClick={() => handleMenuButtonClick("mode4")}
+        >
+          Button4
+        </button>
       </div>
       <div className="game">
         <div className="game-screen"></div>
